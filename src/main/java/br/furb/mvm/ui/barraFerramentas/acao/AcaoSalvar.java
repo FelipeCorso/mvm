d113fa8470c03296bb00992fd9e0977ef760d8f5
Felipe Corso <felipe.corso@live.com>
2016-11-19 21:00:27 -0200

    Cria versão inicial da interface de debug

---
+++ src/main/java/br/furb/mvm/ui/barraFerramentas/acao/AcaoSalvar.java
@@ -0,0 +1,14 @@
+package br.furb.mvm.ui.barraFerramentas.acao;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AcaoSalvar {
+
+    public static void salvar(String absolutePath, String buffer) throws IOException {
+        FileWriter fw = new FileWriter(absolutePath, false);
+        fw.write(buffer);
+        fw.flush();
+        fw.close();
+    }
+}
---
+++ src/main/java/br/furb/mvm/ui/barraFerramentas/botoes/BotaoEquipe.java
@@ -0,0 +1,24 @@
+package br.furb.mvm.ui.barraFerramentas.botoes;
+
+import javax.swing.JButton;
+
+import br.furb.mvm.ui.CompilerInterface;
+import br.furb.mvm.ui.barraFerramentas.acao.Acao;
+
+public class BotaoEquipe extends JButton implements Acao {
+
+    private static final long serialVersionUID = 1L;
+
+    public BotaoEquipe() {
+        super();
+    }
+
+    public BotaoEquipe(String texto) {
+        super(texto);
+    }
+
+    @Override
+    public void executaAcao(CompilerInterface frame) {
+        frame.getTextMsg().setText("Integrantes Equipe: André Luiz Lunelli, Felipe Loose Corso\n");
+    }
+}
